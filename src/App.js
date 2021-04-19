5b9965c7d3dae0511e08cbd7de1413a0f781d0a4
alberto29mGit <alberto29m@gmail.com>
2021-04-19 19:05:41 +0200

    App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
+const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=xVRHX3xGqIi52kV4fXBgfV8xu79Yr5Qh&q=rinoceronte&limit=25&offset=0&rating=g&lang=en';
+
 function App() {
+
+  const [gifs, setGifs] = useState([]);
+  
+  useEffect(function () {
+    console.log('actualizando gifs');
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(response => {
+        const {data} = response;
+        const gifs = data.map(image => image.images.downsized_medium.url);
+        console.log(gifs);
+      })
+
+
+  }, []);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        {
+          gifs.map(singleGif => <img src={singleGif} /> )
+        }
+        
+      </div>
     </div>
   );
 }
